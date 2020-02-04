dce61fe3d25f17a6d8147516d934003522abbd0e
Johan Sjövall <joalsj@utu.fi>
2020-02-04 23:47:29 +0200

    #1 Style updates done

--- lighthouse-app/src/components/Light.js
+++ lighthouse-app/src/components/Light.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import '../styles/light.css'
 
 const Light = ({light, click}) => {
     return(
-    <button onClick={click} >{light.label}</button>
+    <button value={light.id} className={light.power} onClick={click}>{light.label}</button>
     );
 }
 export default Light;
